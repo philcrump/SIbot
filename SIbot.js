3065dfedb8a9a170eb59391f954299b0d447a6a0
Phil Crump <phildcrump@gmail.com>
2013-11-05 22:16:15 +0000

    Accept seperators, use km for >5000m.

--- SIbot.js
+++ SIbot.js
@@ -14,14 +14,16 @@ var bot = new irc.Client(config.server, config.botName, {
 
 bot.addListener('message', function (from, to, message) {
 
-    ft_regex="[0-9]+[ ]*f[e]{0,2}t";
+    ft_regex="[0-9]*[, ]?[0-9]+[ ]*f[e]{0,2}t";
     if(message.search(ft_regex) != -1) { // ft/feet
         console.log("Detected feet at "+message.search(ft_regex));
-        sillyNum=message.substr(message.search(ft_regex),12).match("[0-9]+");
+        sillyNum=message.substr(message.search(ft_regex),12).match("[0-9]*[, ]?[0-9]+").replace(",","");
         realNum=parseInt(sillyNum)*0.3048;
         console.log(" - Got Number as "+sillyNum);
         if(realNum<10) {
             outStr="In real units: "+sillyNum+" ft = "+realNum.toFixed(2)+" m";
+        } else if(realNum>5000) {
+            outStr="In real units: "+sillyNum+" ft = "+Math.round(realNum/1000)+" km";
         } else {
             outStr="In real units: "+sillyNum+" ft = "+Math.round(realNum)+" m";
         }
