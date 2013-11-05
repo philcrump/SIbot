211912add3b3ff0329c053015f71c3a7d2ce5531
Phil Crump <phildcrump@gmail.com>
2013-11-05 21:27:04 +0000

    Complete message.

--- SIbot.js
+++ SIbot.js
@@ -18,7 +18,7 @@ bot.addListener('message', function (from, to, message) {
         sillyNum=message.substr(message.search("[0-9]+\s?f[e]{0,2}t"),12).match("[0-9]+");
         realNum=parseInt(sillyNum)*0.3048;
         console.log(" - Got Number as "+sillyNum);
-        bot.say(config.channels[0], "In real units: "+sillyNum+"ft = ");
+        bot.say(config.channels[0], "In real units: "+sillyNum+"ft = "+realNum+"m");
     }
 });
 
