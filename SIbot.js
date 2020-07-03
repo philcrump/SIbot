65fcaca84eb91e33a8bb371c240b43474f451ef0
Phil Crump <phildcrump@gmail.com>
2020-07-03 16:27:51 +0000

    Add inches (been there a while) and improve regexes and output wording slightly.

--- SIbot.js
+++ SIbot.js
@@ -18,29 +18,48 @@ bot.addListener('message', function (from, to, message) {
     // (more than one conversion can be output per input message)
     
     feet_parse(message, to); // ft / feet
+    inch_parse(message, to); // in / inch
     pounds_parse(message, to); // lb / lbs
 });
 
 function feet_parse(message, to) {
-    ft_regex="[0-9]*[,]?[0-9]+[ ]*f[e]{0,2}t";
+    ft_regex="(?<![\w\/])[0-9]*[,]?[0-9]+[ ]*(\'|f[e]{0,2}t)(?!\w)";
     if(message.search(ft_regex) != -1) {
         console.log(new Date()+": Detected feet at "+message.search(ft_regex));
         sillyNum=message.substr(message.search(ft_regex),12).match("[0-9]*[,]?[0-9]+");
         realNum=parseFloat(String(sillyNum).replace(",",""))*0.3048;
         console.log(" - Got Number as "+sillyNum);
         if(realNum<10) {
-            outStr="In real units: "+sillyNum+" ft = "+realNum.toFixed(2)+" m";
+            outStr="In real units: "+sillyNum+" feet = "+realNum.toFixed(2)+" m";
         } else if(realNum>15000) {
-            outStr="In real units: "+sillyNum+" ft = "+(Math.round(realNum/100)/10) +" km";
+            outStr="In real units: "+sillyNum+" feet = "+(Math.round(realNum/100)/10) +" km";
         } else {
-            outStr="In real units: "+sillyNum+" ft = "+Math.round(realNum)+" m";
+            outStr="In real units: "+sillyNum+" feet = "+Math.round(realNum)+" m";
+        }
+        bot.say(to, outStr);
+    }
+}
+
+function inch_parse(message, to) {
+    in_regex="(?<![\w\/])[0-9]*[,]?[0-9]+[ ]*(\"|in|inch)(es)?(?!\w)";
+    if(message.search(in_regex) != -1) {
+        console.log(new Date()+": Detected inch at "+message.search(in_regex));
+        sillyNum=message.substr(message.search(in_regex),12).match("[0-9]*[,]?[0-9]+");
+        realNum=parseFloat(String(sillyNum).replace(",",""))*2.54;
+        console.log(" - Got Number as "+sillyNum);
+        if(realNum<50) {
+            outStr="In real units: "+sillyNum+" inches = "+realNum.toFixed(2)+" cm";
+        } else if(realNum<2000) {
+            outStr="In real units: "+sillyNum+" inches = "+(Math.round(realNum/10)/10) +" m";
+        } else {
+            outStr="In real units: "+sillyNum+" inches = "+Math.round(realNum/100)+" m";
         }
         bot.say(to, outStr);
     }
 }
 
 function pounds_parse(message, to) {
-    lb_regex="[0-9]*[,]?[.]?[0-9]+[ ]*lb";
+    lb_regex="(?<![\w\/])[0-9]*[,]?[.]?[0-9]+[ ]*lb(?!\w)";
     if(message.search(lb_regex) != -1) {
         console.log(new Date()+": Detected pounds at "+message.search(lb_regex));
         sillyNum=message.substr(message.search(lb_regex),12).match("[0-9]*[,]?[.]?[0-9]+");
